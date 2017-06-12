d6f98651fa89a4cf072eb5e466c2020ca0b98904
Student13 <weerapatit122@gmail.com>
2017-06-12 16:13:38 +0700

    print

--- Variable and Constant.playground/Contents.swift
+++ Variable and Constant.playground/Contents.swift
@@ -18,3 +18,63 @@ let myConstant1:  Int = 123
 print(intNumber)        // Display only Value การแสดงผลเฉพาะ ค่า
 
 print("intNumber ==> \(intNumber)")     //DIsplay String and Value
+
+// Explicit String การประกาศ แบบ กำหนด
+//Data Type ==> String
+let strName: String = "Pogamon"
+
+//Implicit String Datatype ==> String
+let strSurname = "Japane"
+
+//Operate
+var strOfficor = strName + " " + strSurname
+strOfficor = "Mr."  + strOfficor
+
+//Operating on Number
+let intA = 5
+let intB = 2
+
+var answer = intA + intB
+answer = intA * intB
+answer = intA / intB
+
+var douA = Double(intA)
+var douB = Double(intB)
+
+var answer2:  Double = douA  /  douB
+
+//Boolean
+var myStatus = true //Impicit
+var myStatus2: Bool = false //Explicit
+
+//print on Console ==> ถ้าเอา 5 บวก 6 เท่ากับ 11
+let strTitle1 = "ถ้าเอา"
+let strTitle2 = "เท่ากับ"
+let strAdd = "บวก"
+let strHand = "หาร"
+let strDelete = "ลบ"
+let strkuun = "คูณ"
+let strSpace = "  "
+let int2 = 6
+let int1 = 5
+
+print( strTitle1 +  strSpace  +  "\(int1)"  +  strSpace  +  strAdd  +  strSpace  +  "\( int2) "  +  strSpace  +  strTitle2  +  strSpace  +  "\( int1 + int2 )" )
+
+print( strTitle1 +  strSpace  +  "\(int1)"  +  strSpace  +  strDelete  +  strSpace  +  "\( int2) "  +  strSpace  +  strTitle2  +  strSpace  +  "\( int1 - int2 )" )
+
+print( strTitle1 +  strSpace  +  "\(int1)"  +  strSpace  +  strkuun  +  strSpace  +  "\( int2) "  +  strSpace  +  strTitle2  +  strSpace  +  "\( int1 * int2 )" )
+
+var dou1 = Double(int1)
+var dou2 = Double(int2)
+print( strTitle1 +  strSpace  +  "\(int1)"  +  strSpace  +  strHand  +  strSpace  +  "\( int2) "  +  strSpace  +  strTitle2  +  strSpace  +  "\( dou1 / dou2 )" )
+
+
+
+
+
+
+
+
+
+
+
